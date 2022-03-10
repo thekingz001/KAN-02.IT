e68e623133f45bcc51e9d246c8bce0465aba5b11
thekingz001 <57693337+thekingz001@users.noreply.github.com>
2022-03-10 10:54:21 +0700

    Update condb.php

--- condb.php
+++ condb.php
@@ -1,6 +1,6 @@
 <?php
 // DB from Vansale
-$con= mysqli_connect("kns-database21.cu4s2ibvwwoe.ap-southeast-1.rds.amazonaws.com:3306","nps_user","#2r&xVgOwF6","nps") or die("Error: " . mysqli_error($con));
+$con= mysqli_connect("kns-database21.cu4s2ibvwwoe.ap-southeast-1.rds.amazonaws.com:3306","nps_user","","nps") or die("Error: " . mysqli_error($con));
 mysqli_query($con, "SET NAMES 'utf8' ");
 error_reporting( error_reporting() & ~E_NOTICE ); 
 
